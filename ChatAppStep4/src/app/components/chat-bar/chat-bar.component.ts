041d63d943a12bcf68cc43b3445db1c1824d717e
Buttazoni, Christian <christian.buttazoni@unisg.ch>
2020-04-16 20:36:05 +0200

    add

---
+++ ChatAppStep4/src/app/components/chat-bar/chat-bar.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-bar',
+  templateUrl: './chat-bar.component.html',
+  styleUrls: ['./chat-bar.component.css']
+})
+export class ChatBarComponent implements OnInit {
+
+  public chatMessage: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public addMessage(message: string): void {
+    if (!message) {
+      alert('Bitte Text erfassen!');
+      return;
+    }
+
+    alert(message);
+    this.chatMessage = '';
+  }
+}
