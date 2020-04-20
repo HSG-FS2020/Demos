59d02ec2a30d829772251f864672cedf22947775
Buttazoni, Christian <christian.buttazoni@unisg.ch>
2020-04-20 17:57:14 +0200

    add

--- ChatAppStep4/src/app/app.component.ts
+++ ChatAppStep4/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'ChatApp';
+  public title = "ChatApp";
+  public messageHistory: string = "";
+
+  public onSubmitMessage(message: string): void {
+    this.messageHistory += message;
+  }
 }
